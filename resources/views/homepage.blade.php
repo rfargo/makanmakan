ebd8f65533e8e88ccee630d36ebf4b47f22f8c4e
Unknown <renaldy_fargo@yahoo.com>
2018-03-28 04:54:04 +0700

    Added Hover.css and Css

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,41 +2,129 @@
 <html>
 <head>
 	<title>| makanmakan</title>
+
+	<!--Bootsrap css-->
     <link rel="stylesheet" href="{{ asset('assets/bootstrap-4.0.0/dist/css/bootstrap.min.css') }}">
+
+  <!--Hovermaster css-->
+    <link rel="stylesheet" href="{{ asset('assets/Hover-master/css/hover.css') }}">
+
+	<!--Page css-->
     <link rel="stylesheet" href="{{ asset('assets/homepage-style.css') }}">
 
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
 
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-company-blue">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+
+  <div class="collapse navbar-collapse float-right" id="navbarArea">
+    <ul class="navbar-nav" id="navbarOption">
+
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link hvr-underline-from-center" href="#">Recipes & Menus</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+
+      <li class="nav-item active">
+        <a class="nav-link hvr-underline-from-center" href="#">Expert's Advices</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+
+      <li class="nav-item active">
+        <a class="nav-link hvr-underline-from-center" href="#">Sign In</a>
       </li>
     </ul>
   </div>
 </nav>
 
 <div class="jumbotron">
-	<div class="container">
-		<h1>hdbajdb</h1>
+	<div class="container-fluid">
+		<h1>makanmakan</h1>
+		<form class="form-inline">
+			<div class="form-group mx-sm-3 mb-2">
+    			<input type="text" name="search-food" class="form-control form-control-lg" id="inputOption" placeholder="What to cook today?">
+			</div>
+			<button type="submit" class="btn btn-warning btn-lg mb-2 hvr-sweep-to-right">Search</button>
+		</form>
+		<p><ins>Advanced Search</ins></p>
 	</div>
 </div>
+
+<div class="container" id="trending-container">
+  <div class="row">
+    <div class="col-4">
+      <h2>Trending Recipe</h2>
+    </div>
+    <div class="col-8">
+      <hr>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-3 trending-food">
+      <div class="trending-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 trending-food">
+      <div class="trending-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 trending-food">
+      <div class="trending-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 trending-food">
+      <div class="trending-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="container" id="newest-container">
+  <div class="row">
+    <div class="col-8">
+      <hr>
+    </div>
+    <div class="col-4">
+      <h2>Newest Recipe</h2>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col-3 newest-food">
+      <div class="newest-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 newest-food">
+      <div class="newest-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 newest-food">
+      <div class="newest-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+    <div class="col-3 newest-food">
+      <div class="newest-food-write">
+      <h3>Ramen Ayam</h3>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 </body>
 </html>
